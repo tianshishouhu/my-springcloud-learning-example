8ed0f0404c13550da01affb9c944007d43836320
wangjian309@126.com <wangjian309@126.com>
2017-09-23 19:14:15 +0800

    增加客户端参数

--- springcloud-eureka-cluster/springcloud-eureka-cluster-client-customer/src/main/java/org/spring/springcloud/web/CustomerController.java
+++ springcloud-eureka-cluster/springcloud-eureka-cluster-client-customer/src/main/java/org/spring/springcloud/web/CustomerController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -21,11 +22,11 @@ public class CustomerController {
     private RestTemplate restTemplate; // HTTP 访问操作类
 
     @RequestMapping("/customer")
-    public String customer() {
+    public String customer(@RequestParam String name) {
         String providerMsg = restTemplate.getForEntity("http://CLUSTER-PROVIDER-SERVICE/provider",
                 String.class).getBody();
 
-        return "Hello,Customer! msg from provider : <br/><br/> " + providerMsg;
+        return "Hello,Customer! msg from provider : <br/><br/> " + providerMsg + name;
     }
 
 }
